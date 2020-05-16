a599dcd8d841da32afac03bede42c19397f99fc0
Alessio <alessio.caciagli@okra.ai>
2020-05-16 13:34:40 +0100

    initial commenting

--- algorithm.py
+++ algorithm.py
@@ -14,23 +14,30 @@ GREEN = "#32CD32"
 
 def plot_line(ax, ob, color=BLUE):
     """Function to plot Shapely line object"""
+    # Get objects x and y boundary as line
     x, y = ob.xy
     ax.plot(x, y, color=color, linewidth=3, solid_capstyle="round", zorder=1, alpha=0.5)
 
 
 def plot_coords(ax, ob, color=GRAY):
     """Function to plot Shapely object coordinates"""
+    # Get Shapely object point coords (e.g. polygon vertexes)
     coords = np.asarray(ob)
+    #  If multiple points, scatter plot
     if coords.size > 2:
         ax.scatter(coords[:, 0], coords[:, 1], color=color)
+    # If single, point plot
     else:
         ax.scatter(coords[0], coords[1], color=color)
 
 
 def generate_random(polygon):
     """Generate a random point inside a polygon."""
+    # Get cartesian boundaries of polygon
     minx, miny, maxx, maxy = polygon.bounds
+    # Create a random point inside a square defined by the cartesian boundaries
     pnt = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
+    # Check for point being inside polygon. If not, generate another random point
     while not polygon.contains(pnt):
         pnt = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
     return pnt
@@ -41,15 +48,22 @@ def populate_square(ob, iters=1000, r=1):
     It performs "iters" attemps of disk insertion.
     It returns an array of the coordinates of the inserted disk centers.
     """
+    # Define array of points
     pts = np.zeros(shape=(iters, 2))
-    accept = 0
+    # Initialize first point -- always accepted as it's the first!
     pts[0, :] = np.asarray(generate_random(ob))
-    accept += 1
+    accept = 1
+    # Do n=iters disk insert attempt
     for i in tqdm(range(iters)):
+        #  Generate random point inside ob
         pts_temp = np.asarray(generate_random(ob))
+        # Calculate cartesian difference between pts_temp and all existing points in pts array
         pts_diff = pts[:accept] - pts_temp
+        # Perform overlap boolean check with all existing points in pts array
         euclid_bool = (pts_diff * pts_diff).sum(1) > 4 * r * r
+        # Check that pts_temp doesn't overlap with any other
         if np.all(euclid_bool):
+            # If all checks are positive, add point
             pts[accept, :] = pts_temp
             accept += 1
 
