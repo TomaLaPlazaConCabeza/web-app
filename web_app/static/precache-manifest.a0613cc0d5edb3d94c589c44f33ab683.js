b3eeec29b845f62cad204c541d21afad3f7ccbc0
Grazvydas Lucinskas <grazvydaslucinskas@Grazs-MBP.lan>
2020-05-16 12:55:14 +0100

    ejected

---
+++ web_app/static/precache-manifest.a0613cc0d5edb3d94c589c44f33ab683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0bdc0edab10b476dc600e034a5251c",
+    "url": "/../templates/index.html"
+  },
+  {
+    "revision": "c9285190d3daae0676b5",
+    "url": "/css/main.33d251c7.chunk.css"
+  },
+  {
+    "revision": "bc8dd032403f603d54f9",
+    "url": "/js/2.a7843b21.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/js/2.a7843b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9285190d3daae0676b5",
+    "url": "/js/main.64184498.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/js/runtime-main.82a2a924.js"
+  }
+]);
