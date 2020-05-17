93477af5b0d2ef609e55b323018c746ce63b4957
Sander Bollen <sander@sndrtj.eu>
2020-05-17 21:15:38 +0200

    new text

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -8,7 +8,7 @@ class Header extends PureComponent {
   render() {
     return (
       <AppBar className={style.wrapper} position="static" >
-        <p>Reconquer your squares - with your brain</p>
+        <p>Reconquer your plazas - with your brain</p>
       </AppBar>
     );
   }
--- react-app/src/pages/Home/Home.js
+++ react-app/src/pages/Home/Home.js
@@ -5,32 +5,22 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Shut up and take my money!</h1>
-        <p>Say it in Russian! I don't want to be rescued. You'll have all the Slurm you can drink when you're partying with Slurms McKenzie! Switzerland is small and neutral! We are more like Germany, ambitious and misunderstood!</p>
-        <p>I feel like I was mauled by Jesus. Humans dating robots is sick. You people wonder why I'm still single? <strong> It's 'cause all the fine robot sisters are dating humans!</strong> <em> And I'm his friend Jesus.</em> And when we woke up, we had these bodies.</p>
-        <h2>And when we woke up, we had these bodies.</h2>
-        <p>Oh, all right, I am. But if anything happens to me, tell them I died robbing some old man. You can see how I lived before I met you. You wouldn't. Ask anyway! Yes, except the Dave Matthews Band doesn't rock.</p>
+        <h1>Introduction</h1>
+        <p>A gradual and secure return to our public spaces is paramount in this first phase of the halt to the lockdown due to the outbreak of COVID-19 worldwide. At this moment, any tool towards a simple, rational, and versatile re-design of our public spaces is an immediate necessity. Here, we present a model for predicting the capacity of plazas constrained to social distancing. In order to somehow mimic the free will of individuals, we choose to generate very loose random packings in the confined area defined by the perimeter of the plaza. Individuals are modelled as hard discs whose radius is user-defined. Further information about the algorithm set-up is available free of charge <a href="https://arxiv.org/abs/2005.07038">here</a>.</p>
+        <h2>The current web-app allows:</h2>
+        <p>An intuitive user interface. Draw your favorite plaza, collect the COVID-approved population.</p>
+        <p>Add obstacles by simple drawing them in the plaza.</p>
+        <p>Closing the plaza and set an event on it (e.g., a Sunday market, a concert, etc)</p>
+        <p>Future implementations considered can be found below the interactive map.</p>
+        <h2>Future implementations</h2>
         <ol>
-          <li>No! The kind with looting and maybe starting a few fires!</li><li>Of all the friends I've had… you're the first.</li><li>Whoa a real live robot; or is that some kind of cheesy New Year's costume?</li>
+            <li>Beyond Stochastic: Langevin dynamics of the people in the plazas.</li>
+            <li>Love is in the air: Accounting for couples as rod-like particles.</li>
+            <li>Softening the exclusion zone: Accounting for soft discs instead of hard discs.</li>
+            <li>Panic!: Implementation of escape routes.</li>
+            <li>Public vs private: the algorithm will work the same in a private space, provided the floor map from the pertinent authority (e.g., social event organizers, lab-spaces, classrooms, etc). Please, feel free to reach us.</li>
         </ol>
-
-        <h3>I didn't ask for a completely reasonable excuse! I asked you to get busy!</h3>
-        <p>I'm just glad my fat, ugly mama isn't alive to see this day. What are their names? WINDMILLS DO NOT WORK THAT WAY! GOOD NIGHT! I haven't felt much of anything since my guinea pig died. Dr. Zoidberg, that doesn't make sense. But, okay!</p>
-        <ul>
-          <li>Hey, whatcha watching?</li><li>I feel like I was mauled by Jesus.</li><li>You guys aren't Santa! You're not even robots. How dare you lie in front of Jesus?</li>
-        </ul>
-
-        <p>It's a T. It goes "tuh". I videotape every customer that comes in here, so that I may blackmail them later. Say it in Russian! Ooh, name it after me!</p>
-        <p>You've killed me! Oh, you've killed me! Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! I suppose I could part with 'one' and still be feared…</p>
-        <p>Guards! Bring me the forms I need to fill out to have her taken away! Also Zoidberg. And yet you haven't said what I told you to say! How can any of us trust you? It's toe-tappingly tragic!</p>
-        <p>Fry, we have a crate to deliver. But, like most politicians, he promised more than he could deliver. Can we have Bender Burgers again? Good news, everyone! I've taught the toaster to feel love!</p>
-        <p>Bender, this is Fry's decision… and he made it wrong. So it's time for us to interfere in his life. That's not soon enough! You're going to do his laundry? Interesting. No, wait, the other thing: tedious.</p>
-        <p>Yeah. Give a little credit to our public schools. Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat. Hey, whatcha watching? Oh right. I forgot about the battle.</p>
-        <p>Ooh, name it after me! Stop! Don't shoot fire stick in space canoe! Cause explosive decompression! Soothe us with sweet lies. I was having the most wonderful dream. Except you were there, and you were there, and you were there!</p>
-        <p>Hey! I'm a porno-dealing monster, what do I care what you think? Moving along… Michelle, I don't regret this, but I both rue and lament it. Shut up and take my money! Spare me your space age technobabble, Attila the Hun!</p>
-        <p>But, like most politicians, he promised more than he could deliver. Fry, we have a crate to deliver. Yes! In your face, Gandhi! Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo!</p>
-        <p>And then the battle's not so bad? It's toe-tappingly tragic! Hey, tell me something. You've got all this money. How come you always dress like you're doing your laundry? But I know you in the future. I cleaned your poop.</p>
-        <p>I'm sorry, guys. I never meant to hurt you. Just to destroy everything you ever believed in. Now what? Ummm…to eBay? This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry!</p>
+        <p>please feel free to reach us to extend this list at <a mailto="comunicacion@ayudadigitalcovid.org">comunicacion@ayudadigitalcovid.org</a></p>
       </div>
     );
   }
