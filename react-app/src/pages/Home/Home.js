8508a0ff3f1e8e3cd08fb7f6a4d5ccbab37961a2
Sander Bollen <sander@sndrtj.eu>
2020-05-17 21:49:50 +0200

    more fixes.

--- react-app/src/pages/Home/Home.js
+++ react-app/src/pages/Home/Home.js
@@ -6,7 +6,9 @@ class Home extends Component {
     return (
       <div>
         <h1>Introduction</h1>
-        <p>A gradual and secure return to our public spaces is paramount in this first phase of the halt to the lockdown due to the outbreak of COVID-19 worldwide. At this moment, any tool towards a simple, rational, and versatile re-design of our public spaces is an immediate necessity. Here, we present a model for predicting the capacity of plazas constrained to social distancing. In order to somehow mimic the free will of individuals, we choose to generate very loose random packings in the confined area defined by the perimeter of the plaza. Individuals are modelled as hard discs whose radius is user-defined. Further information about the algorithm set-up is available free of charge <a href="https://arxiv.org/abs/2005.07038">here</a>.</p>
+        <p>A gradual and secure return to our public spaces is paramount in this first phase of the halt to the lockdown due to the outbreak of COVID-19 worldwide. At this moment, any tool towards a simple, rational, and versatile re-design of our public spaces is an immediate necessity. Here, we present an app for predicting the capacity of plazas constrained to social distancing. In order to somehow mimic the free will of individuals, we choose to generate very loose random packings in the confined area defined by the perimeter of the plaza. Individuals are treated as hard discs whose radius is user-defined. </p>
+        <p>Further information about the model is available free of charge at:</p>
+        <p><a href="https://arxiv.org/abs/2005.07038">https://arxiv.org/abs/2005.07038</a></p>
         <h2>The current web-app allows:</h2>
         <p>An intuitive user interface. Draw your favorite plaza, collect the COVID-approved population.</p>
         <p>Add obstacles by simple drawing them in the plaza.</p>
@@ -20,7 +22,7 @@ class Home extends Component {
             <li>Panic!: Implementation of escape routes.</li>
             <li>Public vs private: the algorithm will work the same in a private space, provided the floor map from the pertinent authority (e.g., social event organizers, lab-spaces, classrooms, etc). Please, feel free to reach us.</li>
         </ol>
-        <p>please feel free to reach us to extend this list at <a mailto="comunicacion@ayudadigitalcovid.org">comunicacion@ayudadigitalcovid.org</a></p>
+        <p>please feel free to reach us to extend this list at <a>comunicacion@ayudadigitalcovid.org</a></p>
       </div>
     );
   }
