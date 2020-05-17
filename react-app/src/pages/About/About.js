9d7b722bba104dbc24a571897c1cad0d431454d1
Grazvydas Lucinskas <grazvydaslucinskas@Grazs-MBP.lan>
2020-05-17 02:16:02 +0100

    features

--- react-app/src/pages/About/About.js
+++ react-app/src/pages/About/About.js
@@ -4,7 +4,34 @@ class About extends Component {
 
   render() {
     return (
-      <div>About</div>
+      <div>
+        <h1>As you may or may not know, Lindsay and I have hit a bit of a rough patch.</h1>
+        <p>He'll want to use your yacht, and I don't want this thing smelling like fish. But I bought a yearbook ad from you, doesn't that mean anything anymore? I'm a monster. I'm afraid I just blue myself. Marry me.</p>
+        <p>Say goodbye to these, because it's the last time! As you may or may not know, Lindsay and I have hit a bit of a rough patch. <strong> That's why you always leave a note!</strong> <em> There's only one man I've ever called a coward, and that's Brian Doyle Murray.</em> No, what I'm calling you is a television actor.</p>
+        <h2>No! I was ashamed to be SEEN with you. I like being with you.</h2>
+        <p>I'm a monster. I'm afraid I just blue myself. I don't understand the question, and I won't respond to it. Did you enjoy your meal, Mom? You drank it fast enough. I don't understand the question, and I won't respond to it.</p>
+        <ol>
+          <li>Really? Did nothing cancel?</li><li>Say goodbye to these, because it's the last time!</li><li>I'm afraid I just blue myself.</li>
+        </ol>
+
+        <h3>As you may or may not know, Lindsay and I have hit a bit of a rough patch.</h3>
+        <p>I care deeply for nature. Army had half a day. As you may or may not know, Lindsay and I have hit a bit of a rough patch. What's Spanish for "I know you speak English?"</p>
+        <ul>
+          <li>I'm a monster.</li><li>Army had half a day.</li><li>Army had half a day.</li>
+        </ul>
+
+        <p>Whoa, this guy's straight? I'm afraid I just blue myself. Army had half a day. I've opened a door here that I regret. We just call it a sausage. Did you enjoy your meal, Mom? You drank it fast enough.</p>
+        <p>First place chick is hot, but has an attitude, doesn't date magicians. I hear the jury's still out on science. Really? Did nothing cancel? I'm half machine. I'm a monster. I've opened a door here that I regret.</p>
+        <p>No, I did not kill Kitty. However, I am going to oblige and answer the nice officer's questions because I am an honest man with no secrets to hide. Guy's a pro. As you may or may not know, Lindsay and I have hit a bit of a rough patch.</p>
+        <p>Across from where? Now, when you do this without getting punched in the chest, you'll have more fun. There's so many poorly chosen words in that sentence. I'm half machine. I'm a monster. That's what it said on 'Ask Jeeves.'</p>
+        <p>We just call it a sausage. I'm afraid I just blue myself. It's a hug, Michael. I'm hugging you. Guy's a pro. Get me a vodka rocks. And a piece of toast.</p>
+        <p>He'll want to use your yacht, and I don't want this thing smelling like fish. There's so many poorly chosen words in that sentence. I'm a monster. That's what it said on 'Ask Jeeves.'</p>
+        <p>There's only one man I've ever called a coward, and that's Brian Doyle Murray. No, what I'm calling you is a television actor. That's why you always leave a note! I hear the jury's still out on science.</p>
+        <p>It's called 'taking advantage.' It's what gets you ahead in life. But I bought a yearbook ad from you, doesn't that mean anything anymore? Did you enjoy your meal, Mom? You drank it fast enough. Well, what do you expect, mother?</p>
+        <p>It's a hug, Michael. I'm hugging you. But I bought a yearbook ad from you, doesn't that mean anything anymore? Get me a vodka rocks. And a piece of toast. As you may or may not know, Lindsay and I have hit a bit of a rough patch.</p>
+        <p>I care deeply for nature. I'm afraid I just blue myself. I'm a monster. No, I did not kill Kitty. However, I am going to oblige and answer the nice officer's questions because I am an honest man with no secrets to hide.</p>
+        <p>First place chick is hot, but has an attitude, doesn't date magicians. Bad news. Andy Griffith turned us down. He didn't like his trailer. I'm half machine. I'm a monster. Did you enjoy your meal, Mom? You drank it fast enough.</p>
+      </div>
     );
   }
 
